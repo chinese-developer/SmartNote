dd2109858efb4e35e17e7436f147972a1cdc5537
chinese-developer <privateemailmaster@gmail.com>
2023-05-12 00:53:50 +0800

    -

--- feature_sport/src/main/java/com/smarternote/feature/sport/ListActivity.kt
+++ feature_sport/src/main/java/com/smarternote/feature/sport/ListActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.chad.library.adapter.base.BaseQuickAdapter
@@ -12,7 +11,6 @@ import com.chad.library.adapter.base.viewholder.DataBindingHolder
 import com.smarternote.core.base.activity.BaseActivity
 import com.smarternote.core.base.delegates.contentView
 import com.smarternote.core.config.RouterPath
-import com.smarternote.core.utils.logDebug
 import com.smarternote.feature.sport.databinding.ActivityListBinding
 import com.smarternote.feature.sport.databinding.ItemHorizontalBinding
 import com.smarternote.feature.sport.databinding.ItemListBinding
@@ -43,45 +41,39 @@ class ListActivity : BaseActivity() {
 
     }
 
-    class ListAdapter : BaseQuickAdapter<Title, ListAdapter.VH>(), HorizontalScrollObserver {
+    class ListAdapter : BaseQuickAdapter<Title, ListAdapter.VH>() {
 
-        private val horizontalScrollListener = HorizontalScrollListener()
         private val viewHolders = mutableSetOf<VH>()
+
+        private val onScroll: (dx: Int) -> Unit = { dx ->
+            viewHolders.forEach { viewHolder ->
+                if (viewHolder.binding.horizontalRecyclerView.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
+                    viewHolder.binding.horizontalRecyclerView.scrollBy(dx, 0)
+                }
+            }
+        }
 
         inner class VH(
             parent: ViewGroup,
-            private onScrollListener: OnScrollListener,
             val binding: ItemListBinding = ItemListBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
             )
-        ) : RecyclerView.ViewHolder(binding.root), HorizontalScrollObserver {
+        ) : RecyclerView.ViewHolder(binding.root) {
 
             private val horizontalAdapter = HorizontalAdapter()
 
             fun bind(item: List<String>?) {
                 horizontalAdapter.submitList(item)
+                binding.horizontalRecyclerView.layoutManager = SyncScrollLinearLayoutManager(binding.horizontalRecyclerView.context, onScroll)
                 binding.horizontalRecyclerView.adapter = horizontalAdapter
             }
 
-            override fun onScroll(source: RecyclerView, dx: Int) {
-                logDebug("ViewHolder onScroll")
-                // 如果当前滑动的 RecyclerView 不是源 RecyclerView，则同步横向滑动
-                if (binding.horizontalRecyclerView != source) {
-                    viewHolders.forEach { viewHolder ->
-                        val scroller = CustomLinearSmoothScroller(binding.horizontalRecyclerView.context)
-                        scroller.targetPosition = binding.horizontalRecyclerView.computeHorizontalScrollOffset() + dx
-                        viewHolder.binding.horizontalRecyclerView.layoutManager?.startSmoothScroll(scroller)
-                    }
-                }
-            }
         }
 
         override fun onBindViewHolder(holder: VH, position: Int, item: Title?) {
             holder.bind(item?.childs)
-            holder.binding.horizontalRecyclerView.addOnScrollListener(horizontalScrollListener)
-            horizontalScrollListener.addObserver(holder)
             viewHolders.add(holder)
         }
 
@@ -91,14 +83,7 @@ class ListActivity : BaseActivity() {
 
         override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
             super.onDetachedFromRecyclerView(recyclerView)
-            horizontalScrollListener.observers.clear()
             viewHolders.clear()
-        }
-
-        override fun onScroll(source: RecyclerView, dx: Int) {
-            logDebug("Vertical onScroll")
-            // 通知横向滑动
-            horizontalScrollListener.onScrolled(source, dx, 0)
         }
 
         inner class HorizontalAdapter : BaseQuickAdapter<String, DataBindingHolder<ItemHorizontalBinding>>() {
@@ -112,11 +97,5 @@ class ListActivity : BaseActivity() {
             }
 
         }
-    }
-}
-
-class CustomLinearSmoothScroller(context: Context) : LinearSmoothScroller(context) {
-    override fun getHorizontalSnapPreference(): Int {
-        return SNAP_TO_START
     }
 }
